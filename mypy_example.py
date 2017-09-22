fe6b752623ff6b7ffafa83e21d2944b2a99ec2db
Maxwell Forbes <mbforbes@cs.washington.edu>
2017-09-22 14:36:17 -0700

    minimal example of int vs float tensor not type checking problem

--- mypy_example.py
+++ mypy_example.py
@@ -10,9 +10,18 @@ a, b = foo()
 reveal_type(a)
 reveal_type(b)
 
-def baz() -> Tuple[torch.IntTensor, torch.FloatTensor]:
+def bar() -> Tuple[torch.IntTensor, torch.FloatTensor]:
     return torch.IntTensor(1), torch.FloatTensor(1.0)
 
-c, d = baz()
+c, d = bar()
 reveal_type(c)
 reveal_type(d)
+
+
+def baz(it: torch.IntTensor, ft: torch.FloatTensor) -> None:
+    pass
+
+baz(c, d)  # should be ok
+baz(1, 2)  # should be bad
+baz(c, c)  # should be bad
+baz(d, d)  # should be bad

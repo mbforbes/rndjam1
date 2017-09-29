fix ridge gradient in math and code (had turned - to + during math...).
also make look similar to ols form for clarity as to what's happening.

regression.py
@@ -122,7 +122,8 @@ def ols_gradient(w: FloatTensor, x: FloatTensor, y: FloatTensor, _: float) -> Fl
     """
     n, d = x.size()
     x_t = x.t()
-    return (2/n)*(x_t.matmul(x).matmul(w) - x_t.matmul(y))
+    return (2/n)*(x_t.matmul(x.matmul(w) - y))
+
 
 
 #
@@ -192,7 +193,7 @@ def ridge_gradient(w: FloatTensor, x: FloatTensor, y: FloatTensor, lmb: float) -
     """
     n, d = x.size()
     x_t = x.t()
-    return 2*(lmb*w - (x_t.matmul(y) + x_t.matmul(x).matmul(w))/n)
+    return 2*lmb*w + (2/n)*(x_t.matmul(x.matmul(w) - y))
 
 
 #
@@ -323,16 +324,16 @@ val_x: torch.cuda.FloatTensor = val_x_cpu.cuda()
 print('Starting experiments...')
 dummy = 0.0
 
-# # OLS analytic solution. uses CPU tensors to go to/from numpy for pseudoinverse.
-# w = ols_analytic(train_x_cpu, train_y_cpu)
-# naive_regression_eval('OLS analytic (train)', w, train_x, train_y, dummy, ols_loss)
-# naive_regression_eval('OLS analytic (val)', w, val_x, val_y, dummy, ols_loss)
+# OLS analytic solution. uses CPU tensors to go to/from numpy for pseudoinverse.
+w = ols_analytic(train_x_cpu, train_y_cpu)
+naive_regression_eval('OLS analytic (train)', w, train_x, train_y, dummy, ols_loss)
+naive_regression_eval('OLS analytic (val)', w, val_x, val_y, dummy, ols_loss)
 
-# # OLS gradient descent
-# ols_gd_settings: GDSettings = {'lr': 0.022, 'epochs': 1500, 'report_interval': 100}
-# w = gradient_descent_regression(train_x, train_y, -1, ols_loss, ols_gradient, ols_gd_settings)
-# naive_regression_eval('OLS GD (train)', w, train_x, train_y, dummy, ols_loss)
-# naive_regression_eval('OLS GD (val)', w, val_x, val_y, dummy, ols_loss)
+# OLS gradient descent
+ols_gd_settings: GDSettings = {'lr': 0.02, 'epochs': 1500, 'report_interval': 100}
+w = gradient_descent_regression(train_x, train_y, -1, ols_loss, ols_gradient, ols_gd_settings)
+naive_regression_eval('OLS GD (train)', w, train_x, train_y, dummy, ols_loss)
+naive_regression_eval('OLS GD (val)', w, val_x, val_y, dummy, ols_loss)
 
 # ridge analytic solution
 for lmb in [0.2]:
@@ -342,7 +343,7 @@ for lmb in [0.2]:
     naive_regression_eval('Ridge analytic (val) lambda={}'.format(lmb), w, val_x, val_y, lmb, ridge_loss)
 
 # ridge GD
-ridge_gd_settings: GDSettings = {'lr': 0.0001, 'epochs': 200, 'report_interval': 10}
+ridge_gd_settings: GDSettings = {'lr': 0.02, 'epochs': 500, 'report_interval': 100}
 for lmb in [0.2]:
     w = gradient_descent_regression(train_x, train_y, lmb, ridge_loss, ridge_gradient, ridge_gd_settings)
     naive_regression_eval('Ridge GD (train) lambda={}'.format(lmb), w, train_x, train_y, lmb, ridge_loss)